135e7628e1b3b346ca58ef43c185d7b211b87c50
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2023-01-24 20:11:49 -0300

    feat: project inicialization

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+export default function App(): JSX.Element {
+  return <></>;
+}
